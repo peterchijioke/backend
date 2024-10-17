71ce677d947487b38ffddec5aedf631d550b2513
Chukwu (Chijioke)  Peter <peterchijioke1@gmail.com>
2024-10-17 23:18:39 +0100

    Update .gitignore and remove ignored files from tracking

--- src/utils/httpError.ts
+++
@@ -1,9 +0,0 @@
-export class HttpError extends Error {
-  status: number;
-
-  constructor(message: string, status: number) {
-    super(message); 
-    this.status = status;
-    this.name = this.constructor.name; 
-  }
-}
